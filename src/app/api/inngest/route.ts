1a4a043f6aedadf8abfa6202ec74762c41fda22d
kalamanch <thekalamanchofficial@gmail.com>
2024-12-20 15:56:57 +0530

    update user

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { syncUser } from "~/inngest/syncUser";
+import { updateUser } from "~/inngest/updateUser";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [syncUser],
+  functions: [syncUser, updateUser],
 });
