6a0ec62b0a0017d53face3cd22ee368db7c21fc9
Yash Rajoria <yashrajoria03@gmail.com>
2024-12-21 21:23:13 +0530

    fixed bug

--- src/app/(with-sidebar)/myProfile/_schema/editProfileSchema.ts
+++ src/app/(with-sidebar)/myProfile/_schema/editProfileSchema.ts
@@ -8,7 +8,9 @@ export const editProfileSchema = Yup.object().shape({
     .max(new Date(), "Date of birth can't be in the future")
     .required("Birthdate is required"),
   bio: Yup.string().optional(),
-  interests: Yup.array().min(1, "At least one interests are required"),
+  interests: Yup.array()
+    .min(1, "At least one interests are required")
+    .required(),
   education: Yup.array().optional(),
   professionalAchievements: Yup.string().optional(),
 });
