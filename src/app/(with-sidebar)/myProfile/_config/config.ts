146a34dd8ebf0f488f3236b8dfd60b149d964110
Yash Rajoria <yashrajoria03@gmail.com>
2024-12-21 21:31:11 +0530

    fixing file name

--- src/app/(with-sidebar)/myProfile/_config/config.ts
+++
@@ -1,9 +0,0 @@
-import { MyProfileTabsEnum } from "../types/types";
-
-export const tabs: { label: string; value: MyProfileTabsEnum }[] = [
-  { label: MyProfileTabsEnum.MY_POSTS, value: MyProfileTabsEnum.MY_POSTS },
-  {
-    label: MyProfileTabsEnum.LIKED_POSTS,
-    value: MyProfileTabsEnum.LIKED_POSTS,
-  },
-];
