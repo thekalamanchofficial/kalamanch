2da07250ff9923736f0fad76b381a95be430133a
kalamanch <thekalamanchofficial@gmail.com>
2025-01-30 00:03:28 +0530

    cleanup

--- src/app/_components/analytics/Analytics.tsx
+++ src/app/_components/analytics/Analytics.tsx
@@ -1,27 +1,10 @@
 "use client"; // Ensures it runs only on the client side
 
-import { useEffect } from "react";
 import Script from "next/script";
 
 const GA_TRACKING_ID = process.env.GA_TRACKING_ID; // Replace with your GA ID
 
 export default function Analytics() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Define `window.dataLayer` to avoid TypeScript errors
-      (window as any).dataLayer = (window as any).dataLayer || [];
-
-      function gtag(...args: any) {
-        (window as any).dataLayer.push(args);
-      }
-
-      gtag("js", new Date());
-      gtag("config", GA_TRACKING_ID, {
-        page_path: window.location.pathname,
-      });
-    }
-  }, []);
-
   return (
     <>
       {/* Google Analytics Script */}
